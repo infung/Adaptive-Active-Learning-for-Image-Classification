d293fa4ac148050a97a1359ca607dd37c01078b8
Yan Feng <36089888+infung@users.noreply.github.com>
2023-11-09 00:42:05 +0800

    Add Caltech split tool

--- start.py
+++ start.py
@@ -3,17 +3,27 @@ from scipy.io import loadmat
 from Adaptive_learning.activeLearning import activecombsample
 
 # Load the data from MATLAB files
-data = loadmat('voc07_densesift.mat')
-voc07_densesift = data['feats']
+# data = loadmat('voc07_densesift.mat')
+# voc07_densesift = data['feats']
+#
+# data = loadmat('voc_labelset.mat')
+# labelset = data['labelset']
+#
+# data = loadmat('voc_unlabelset.mat')
+# unlabelset = data['unlabelset']
+#
+# data = loadmat('voc_testset.mat')
+# testset = data['testset']
 
-data = loadmat('voc_labelset.mat')
+data = loadmat('caltech_101.mat')
 labelset = data['labelset']
-
-data = loadmat('voc_unlabelset.mat')
 unlabelset = data['unlabelset']
-
-data = loadmat('voc_testset.mat')
 testset = data['testset']
+
+# labelset = np.loadtxt('labelset.csv', delimiter=',')
+# unlabelset = np.loadtxt('unlabelset.csv', delimiter=',')
+# testset = np.loadtxt('testset.csv', delimiter=',')
+
 
 repeatnum = 100  # Selection iterations
 allbeta = np.arange(0, 1.1, 0.1)
