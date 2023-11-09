1f5187e85ccd894f2b89e7712d3cc1313a25796f
Yan Feng <36089888+infung@users.noreply.github.com>
2023-11-09 12:56:32 +0800

    add a program to plot the comparison

---
+++ plotGraph.py
@@ -0,0 +1,68 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy.io import loadmat
+
+from Adaptive_learning.activeLearning import activecombsample
+from Adaptive_learning.otherApproaches import otherApproaches
+
+data = loadmat('voc_labelset.mat')
+labelset = data['labelset']
+
+data = loadmat('voc_unlabelset.mat')
+unlabelset = data['unlabelset']
+
+data = loadmat('voc_testset.mat')
+testset = data['testset']
+
+# data = loadmat('caltech_101.mat')
+# labelset = data['labelset']
+# unlabelset = data['unlabelset']
+# testset = data['testset']
+
+repeatnum = 100
+
+#           1 - single beta with proposed approach
+#           2 - most uncertainty
+#           3 - fixed combination with cosine distance to measure an information density
+approach = {
+    'method': 2,
+    'num': 1  # only useful for method 2, select the top 'num' uncertainty
+}
+beta = 0.5
+adaptiveresult, _ = otherApproaches(labelset, unlabelset, testset, beta, repeatnum, approach)
+accuracy_uncertainty = adaptiveresult
+
+approach['method'] = 3
+beta = 1
+adaptiveresult, _ = otherApproaches(labelset, unlabelset, testset, beta, repeatnum, approach)
+accuracy_fixedComb = adaptiveresult
+
+allbeta = np.arange(0, 1.1, 0.1)
+adaptiveresult, _ = activecombsample(labelset, unlabelset, testset, allbeta, repeatnum)
+accuracy_proposed = adaptiveresult
+
+iteration = np.arange(0, repeatnum+1, 1)
+
+plt.plot(iteration, accuracy_proposed, label='Proposed Approach')
+plt.plot(iteration, accuracy_uncertainty, label='Most Uncertainty')
+plt.plot(iteration, accuracy_fixedComb, label='Fixed Combination')
+plt.xlabel('num')
+plt.ylabel('Classification Accuracy')
+plt.title('Logistic Regression Classification Accuracy')
+plt.legend()
+plt.show()
+
+approach['method'] = 1
+accuracy_beta = []
+beta = [0.25,0.5,0.75,1]
+for b in beta:
+    beta = b
+    adaptiveresult, _ = otherApproaches(labelset, unlabelset, testset, beta, repeatnum, approach)
+    labell = 'β = ' + str(beta)
+    plt.plot(iteration, adaptiveresult, label=labell)
+
+plt.xlabel('num')
+plt.ylabel('Classification Accuracy')
+plt.title('Logistic Regression Classification Accuracy')
+plt.legend()
+plt.show()
