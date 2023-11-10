b631b4d3cbf5e30a6d0569b7ae98e2865be4a340
Yan Feng <36089888+infung@users.noreply.github.com>
2023-11-10 17:33:50 +0800

    Update start.py

--- start.py
+++ start.py
@@ -3,27 +3,19 @@ from scipy.io import loadmat
 from Adaptive_learning.activeLearning import activecombsample
 
 # Load the data from MATLAB files
-# data = loadmat('voc07_densesift.mat')
-# voc07_densesift = data['feats']
-#
-# data = loadmat('voc_labelset.mat')
-# labelset = data['labelset']
-#
-# data = loadmat('voc_unlabelset.mat')
-# unlabelset = data['unlabelset']
-#
-# data = loadmat('voc_testset.mat')
-# testset = data['testset']
-
-data = loadmat('caltech_101.mat')
+data = loadmat('voc_labelset.mat')
 labelset = data['labelset']
+
+data = loadmat('voc_unlabelset.mat')
 unlabelset = data['unlabelset']
+
+data = loadmat('voc_testset.mat')
 testset = data['testset']
 
-# labelset = np.loadtxt('labelset.csv', delimiter=',')
-# unlabelset = np.loadtxt('unlabelset.csv', delimiter=',')
-# testset = np.loadtxt('testset.csv', delimiter=',')
-
+# data = loadmat('caltech_101.mat')
+# labelset = data['labelset']
+# unlabelset = data['unlabelset']
+# testset = data['testset']
 
 repeatnum = 100  # Selection iterations
 allbeta = np.arange(0, 1.1, 0.1)
