9270e7210a4875d241c6138de23909978c7381cf
Yan Feng <36089888+infung@users.noreply.github.com>
2023-11-10 17:35:51 +0800

    Update plotGraph.py

--- plotGraph.py
+++ plotGraph.py
@@ -5,21 +5,22 @@ from scipy.io import loadmat
 from Adaptive_learning.activeLearning import activecombsample
 from Adaptive_learning.otherApproaches import otherApproaches
 
-# data = loadmat('voc_labelset.mat')
-# labelset = data['labelset']
-#
-# data = loadmat('voc_unlabelset.mat')
-# unlabelset = data['unlabelset']
-#
-# data = loadmat('voc_testset.mat')
-# testset = data['testset']
-
-data = loadmat('caltech_101.mat')
+data = loadmat('voc_labelset.mat')
 labelset = data['labelset']
+
+data = loadmat('voc_unlabelset.mat')
 unlabelset = data['unlabelset']
+
+data = loadmat('voc_testset.mat')
 testset = data['testset']
 
-repeatnum = 100
+# data = loadmat('caltech_101.mat')
+# labelset = data['labelset']
+# unlabelset = data['unlabelset']
+# testset = data['testset']
+
+repeatnum = 20
+iteration = np.arange(0, repeatnum+1, 1)
 
 #           1 - single beta with proposed approach
 #           2 - most uncertainty
@@ -30,20 +31,16 @@ approach = {
 }
 beta = 0.5
 print('most uncertainty approach')
-adaptiveresult, _ = otherApproaches(labelset, unlabelset, testset, beta, repeatnum, approach)
-accuracy_uncertainty = adaptiveresult
+accuracy_uncertainty, _ = otherApproaches(labelset, unlabelset, testset, beta, repeatnum, approach)
 
 print('fixed combination approach')
 approach['method'] = 3
 beta = 1
-adaptiveresult, _ = otherApproaches(labelset, unlabelset, testset, beta, repeatnum, approach)
-accuracy_fixedComb = adaptiveresult
+accuracy_fixedComb, _ = otherApproaches(labelset, unlabelset, testset, beta, repeatnum, approach)
 
 allbeta = np.arange(0, 1.1, 0.1)
 print('proposed approach')
-adaptiveresult, _ = activecombsample(labelset, unlabelset, testset, allbeta, repeatnum)
-accuracy_proposed = adaptiveresult
-iteration = np.arange(0, repeatnum+1, 1)
+accuracy_proposed, _ = activecombsample(labelset, unlabelset, testset, allbeta, repeatnum)
 
 plt.plot(iteration, accuracy_proposed, label='Proposed Approach')
 plt.plot(iteration, accuracy_uncertainty, label='Most Uncertainty')
@@ -61,8 +58,8 @@ accuracy_beta = []
 betaList = [0.25, 0.5, 0.75, 1]
 
 for b in betaList:
-    adaptiveresult, _ = otherApproaches(labelset, unlabelset, testset, b, repeatnum, approach)
-    accuracy_beta.append(adaptiveresult)
+    result, _ = otherApproaches(labelset, unlabelset, testset, b, repeatnum, approach)
+    accuracy_beta.append(result)
 
 plt.plot(iteration, accuracy_proposed, label='Proposed Approach')
 plt.plot(iteration, accuracy_beta[0], label='β = 0.25')
